534b3af1e652576c8d4a350dc4292f803d5b3cd1
ivankulbeda <johnnydrago96@gmail.com>
2024-05-29 11:50:10 +0300

    My commets about the commit

---
+++ cypress/e2e/issue-delete.cy.js
@@ -0,0 +1,56 @@
+describe('Issue deleting', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.url().should('eq', `${Cypress.env('baseUrl')}project`).then((url) => {
+      cy.visit(url + '/board');
+      cy.contains('This is an issue of type: Task.').click();
+    });
+  });
+
+  it('Delete the issue', () => {
+    cy.get('[data-testid="modal:issue-details"]')
+      .should('be.visible')
+      .within(() => {
+        cy.get('[data-testid="icon:trash"]').should('be.visible').click();
+      });
+
+    cy.get('[data-testid="modal:confirm"]')
+      .should('be.visible')
+      .within(() => {
+        cy.get('button').contains('Delete issue').click();
+      });
+
+    cy.get('[data-testid="modal:confirm"]').should('not.exist');
+    cy.get('[data-testid="modal:issue-details"]').should('not.exist');
+  });
+});
+
+describe('Issue deletion cancellation', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.url().should('eq', `${Cypress.env('baseUrl')}project`).then((url) => {
+      cy.visit(url + '/board');
+      cy.contains('This is an issue of type: Task.').click();
+    });
+  });
+
+  it('Cancel the deletion of the issue', () => {
+    cy.get('[data-testid="modal:issue-details"]')
+      .should('be.visible')
+      .within(() => {
+        // Click the first trash icon found within the modal
+        cy.get('[data-testid="icon:trash"]').first().should('be.visible').click();
+      });
+
+    cy.get('[data-testid="modal:confirm"]')
+      .should('be.visible')
+      .within(() => {
+        cy.get('button').contains('Cancel').click();
+      });
+
+    cy.get('[data-testid="modal:confirm"]').should('not.exist');
+    cy.get('[data-testid="modal:issue-details"]').should('be.visible');
+    cy.get('[data-testid="icon:close"]').first().should('be.visible').click();
+    cy.get('[data-testid="modal:issue-details"]').should('not.exist');
+  });
+});
